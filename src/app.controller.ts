fc1bb21c93678957cc9c92e9cbd4545c1184eb33
Israel Estrada <programacion@shopeando.mx>
2024-12-12 09:31:05 -0600

    Controller example and getting params from URL

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /slash/';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param() params: any) {
+    return `product ${params.productId}`;
+  }
+
+  @Get('products2/:productId')
+  getProduct2(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and category ${id}`;
   }
 }

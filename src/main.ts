23fa2a0f9e72d6b669498a61b9fc8219fac6fcb7
Israel Estrada <programacion@shopeando.mx>
2024-12-12 18:09:37 -0600

    Evitando valores de mas o alertando si son diferentes al DTOs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // solo me acepta lo definido en el DTO (ignora)
+      forbidNonWhitelisted: true, // alerta y devuelve 403 bad request
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
